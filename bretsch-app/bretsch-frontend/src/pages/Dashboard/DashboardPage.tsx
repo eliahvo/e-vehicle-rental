e0445cc11ccaaa7dacaac5d6152d2560588e559e
Ramon Walther <Ramon.Walther@stud.h-da.de>
2021-01-18 21:05:24 +0100

    Added clustering to dashboard map

--- bretsch-app/bretsch-frontend/src/pages/Dashboard/DashboardPage.tsx
+++ bretsch-app/bretsch-frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,107 +1,68 @@
 import { useEffect, useState } from 'react';
 import { GoogleMap, LoadScript, Marker, MarkerClusterer } from '@react-google-maps/api';
 import { useSnackbar } from 'notistack';
+import { User, Booking, VehicleType, Vehicle } from '../../util/entityInterfaces';
 
 const containerStyle = {
+  height: '800px',
   width: '100%',
-  height: '800px'
 };
 
 const center = {
   lat: 49.871575,
-  lng: 8.651596
+  lng: 8.651596,
 };
-export interface User {
-	userId: number;
-  email: string;
-  hashedPassword: string;
-  userRole: string;
-  firstName: string;
-  lastName: string;
-  birthDate: string;
-  preferedPayment: string;
-  streetPlusNumber: string;
-  city: string;
-  bookings: Booking[];
-  createdAt: string;
-  updatedAt: string;
-}
-export interface Booking {
-  bookingId: number;
-  createdAt: string;
-  endDate: string;
-  paymentStatus: string;
-  price: number;
-  startDate: string;
-  updatedAt: string;
-}
-export interface VehicleType {
-	vehicleTypeId: number;
-  type: string;
-  pricePerMinute: number;
-  minimalBatteryLevel: number;
-  vehicles: Vehicle[];
-  createdAt: string;
-  updatedAt: string;
-}
 
-export interface Vehicle {
-  batteryLevel: number;
-  bookings: Booking[];
-  createdAt: string;
-  licencePlate: string;
-  positionLatitude: string;
-  positionLongitude: string;
-  status: string;
-  updatedAt: string;
-  vehicleId: number;
-  vehicleType: VehicleType;
-}
+const options = {
+  imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m', // m1.png, m2.png, m3.png, m4.png, m5.png and m6.png have to be in that folder
+};
 
 export const DashboardPage = () => {
   const { enqueueSnackbar } = useSnackbar();
   const [vehicles, setVehicles] = useState<Vehicle[]>([]);
 
-  const fetchVehicle = async function() {
+  const fetchVehicle = async () => {
     const vehicleRequest = await fetch(`api/vehicle`, {
-        method: 'GET',
-        headers: { 'content-type': 'application/json' },
-      },
-    );
+      headers: { 'content-type': 'application/json' },
+      method: 'GET',
+    });
     if (vehicleRequest.status === 200) {
-			const vehicleJSON = await vehicleRequest.json();
-			setVehicles(vehicleJSON.data);
-		}
+      const vehicleJSON = await vehicleRequest.json();
+      setVehicles(vehicleJSON.data);
+    } else {
+      enqueueSnackbar(`Error while fetching vehicle data!`, {
+        variant: 'error',
+      });
+    }
   };
   useEffect(() => {
     fetchVehicle();
-  },[]);
+  }, []);
 
   return (
-    <LoadScript
-      googleMapsApiKey=""
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={15}
-      >
-        { /* Child components, such as markers, info windows, etc. */}
-
-        {vehicles.map((vehicle: Vehicle) => {
-          return (
-            <Marker
-              position={{
-                lat: parseFloat(vehicle.positionLatitude),
-                lng: parseFloat(vehicle.positionLongitude),
-              }}
-              onClick={_ => 
-                enqueueSnackbar(`${vehicle.vehicleType.type} bretscht davon!`, { variant: 'success' })}
-              icon={`./icons/${vehicle.vehicleType.type}.png`}
-            />
-          );
-        })}
+    <LoadScript googleMapsApiKey="">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+        <MarkerClusterer options={options}>
+          {(clusterer) =>
+            vehicles.map((vehicle: Vehicle) => (
+              <Marker
+                key={vehicle.vehicleId}
+                position={{
+                  lat: parseFloat(vehicle.positionLatitude),
+                  lng: parseFloat(vehicle.positionLongitude),
+                }}
+                onClick={(_) =>
+                  enqueueSnackbar(`${vehicle.vehicleType.type + vehicle.vehicleId} bretscht davon!`, {
+                    variant: 'success',
+                  })
+                }
+                icon={`./icons/${vehicle.vehicleType.type}.png`}
+                clusterer={clusterer}
+              />
+            ))
+          }
+        </MarkerClusterer>
       </GoogleMap>
     </LoadScript>
-  )
-}
+  );
+};
