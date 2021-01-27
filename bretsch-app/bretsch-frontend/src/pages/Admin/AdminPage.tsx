03123a01b630cb1f0a84721ef72482851864b8ed
Celine Heymel <celine.heymel@stud.h-da.de>
2021-01-28 00:37:29 +0100

    continued with Admin Page: add delete button and progressbar

--- bretsch-app/bretsch-frontend/src/pages/Admin/AdminPage.tsx
+++ bretsch-app/bretsch-frontend/src/pages/Admin/AdminPage.tsx
@@ -1,10 +1,11 @@
 import { Layout } from '../../components/Layout';
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core';
+import { LinearProgress, makeStyles } from '@material-ui/core';
 import styled from 'styled-components';
-import { ColDef, DataGrid, FilterModel } from '@material-ui/data-grid';
+import { ColDef, DataGrid, FilterModel, ValueFormatterParams } from '@material-ui/data-grid';
 import { MyProfile } from '../Profile/ProfilePage';
 import { Vehicle } from '../../util/EntityInterfaces';
+import Button from '@material-ui/core/Button';
 
 // TODO Object destruction -> concret Table
 const useStyles = makeStyles((theme) => ({
@@ -17,14 +18,18 @@ export const MyAdmin = styled.div`
   margin: 5% 10%;
 `;
 
-const riceFilterModel: FilterModel = {
-  items: [{ columnField: 'commodity', operatorValue: 'contains', value: 'rice' }],
-};
-//
+export const BatteryProgressNumber = styled.span`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  text-align: center;
+  color: black;
+`;
 
-/*export function BasicToolbarFilteringGrid() {
-    const { data } = ""
-    });*/
+export const BatteryProgress = styled.progress`
+  width: 100%;
+`;
 
 export const AdminPage = () => {
   const [vehicles, setVehiles] = useState<Vehicle[]>([]);
@@ -57,6 +62,7 @@ export const AdminPage = () => {
       bookings: vehicle.bookings.length,
       type: vehicle.vehicleType.type,
       position: vehicle.positionLatitude + ' / ' + vehicle.positionLongitude,
+      button: <Button variant="contained">Default</Button>,
     });
   }
 
@@ -65,24 +71,49 @@ export const AdminPage = () => {
       <MyAdmin>
         <div className={classes.headings}>Vehicles</div>
         <div style={{ height: 400, width: '100%' }}>
-          <DataGrid
-            columns={[
-              { field: 'id', hide: true },
-              {
-                field: 'license_plate',
-                headerName: 'License Plate',
-                width: 150,
-              },
-              { field: 'status', headerName: 'Status', width: 150 },
-              { field: 'battery', headerName: 'Battery' },
-              { field: 'bookings', headerName: 'Number of bookings' },
-              { field: 'type', headerName: 'Type' },
-              { field: 'position', headerName: 'Position', width: 300},
-              { field: 'button', headerName: 'Button' },
-            ]}
-            rows={vehicleRows}
-            disableColumnMenu
-          />
+          <div style={{ display: 'flex', height: '100%' }}>
+            <div style={{ flexGrow: 1 }}>
+              <DataGrid
+                columns={[
+                  { field: 'id', hide: true },
+                  {
+                    field: 'license_plate',
+                    headerName: 'License Plate',
+                    width: 150,
+                  },
+                  { field: 'status', headerName: 'Status', width: 150 },
+                  {
+                    field: 'battery',
+                    headerName: 'Battery',
+                    width: 100,
+                    renderCell: (params: ValueFormatterParams) => (
+                      <div style={{ position: 'relative' }}>
+                        <BatteryProgress value={params.value.toString()} max="100" />
+                        <BatteryProgressNumber>{params.value.toString()}</BatteryProgressNumber>
+                      </div>
+                    ),
+                  },
+                  { field: 'bookings', headerName: 'Number of bookings' },
+                  { field: 'type', headerName: 'Type' },
+                  { field: 'position', headerName: 'Position', width: 300 },
+                  {
+                    field: '',
+                    headerName: '',
+                    width: 150,
+                    renderCell: (params: ValueFormatterParams) => (
+                      <strong>
+                        <Button variant="outlined" color="secondary" size="small" style={{ marginLeft: 16 }}>
+                          Delete
+                        </Button>
+                      </strong>
+                    ),
+                  },
+                ]}
+                rows={vehicleRows}
+                disableColumnMenu
+              />
+            </div>
+          </div>
         </div>
       </MyAdmin>
     </Layout>
