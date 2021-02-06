324ffa94c5620f691a480b774eb828a060e6afdd
Celine Heymel <celine.heymel@stud.h-da.de>
2021-02-06 22:33:50 +0100

    add delete button for vehicle (admin page)

--- bretsch-app/bretsch-frontend/src/pages/Admin/components/vehicleTable.tsx
+++ bretsch-app/bretsch-frontend/src/pages/Admin/components/vehicleTable.tsx
@@ -3,6 +3,8 @@ import Button from '@material-ui/core/Button';
 import React, { useEffect, useState } from 'react';
 import { Vehicle } from '../../../util/EntityInterfaces';
 import styled from 'styled-components';
+import { Alert } from '@material-ui/lab';
+import { Snackbar } from '@material-ui/core';
 
 export const BatteryProgressNumber = styled.span`
   position: absolute;
@@ -26,6 +28,7 @@ export const Progressbar = styled.div`
 
 export const VehicleTable = () => {
   const [vehicles, setVehicles] = useState<Vehicle[]>([]);
+  const [open, setOpen] = React.useState(false);
 
   useEffect(() => {
     allVehicles();
@@ -38,8 +41,22 @@ export const VehicleTable = () => {
       method: 'GET',
     });
     if (vehicleRequest.status === 200) {
-      const userJSON = await vehicleRequest.json();
-      setVehicles(userJSON.data);
+      const vehicleJSON = await vehicleRequest.json();
+      setVehicles(vehicleJSON.data);
+    }
+  };
+
+  // delete vehicles
+  const deleteVehicles = async (id: string) => {
+    // tslint:disable-next-line:prefer-template
+    console.log(id);
+    const vehicleRequest = await fetch(`/api/vehicle/` + id, {
+      headers: { 'content-type': 'application/json' },
+      method: 'DELETE',
+    });
+    if (vehicleRequest.status === 204) {
+      await allVehicles();
+      setOpen(true);
     }
   };
 
@@ -54,9 +71,22 @@ export const VehicleTable = () => {
       bookings: vehicle.bookings.length,
       type: vehicle.vehicleType.type,
       position: vehicle.positionLatitude + ' / ' + vehicle.positionLongitude,
-      button: <Button variant="contained">Default</Button>,
+      button: vehicle.vehicleId,
     });
   }
+
+  const handleDeleteVehicle = async (e: any) => {
+    let id = '';
+    id = e.currentTarget.id;
+    await deleteVehicles(id);
+  };
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
   // Filter model for vehicle table
   let vehicleFilterModel: FilterModel;
@@ -65,54 +95,68 @@ export const VehicleTable = () => {
   };
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <div style={{ display: 'flex', height: '100%' }}>
-        <div style={{ flexGrow: 1 }}>
-          <DataGrid
-            columns={[
-              { field: 'id', hide: true },
-              {
-                field: 'license_plate',
-                headerName: 'License Plate',
-                width: 150,
-              },
-              { field: 'status', headerName: 'Status', width: 150 },
-              {
-                field: 'battery',
-                headerName: 'Battery',
-                width: 100,
-                renderCell: (params: ValueFormatterParams) => (
-                  <div style={{ position: 'relative' }}>
-                    <Progressbar>
-                      <BatteryProgress value={params.value.toString()} max="100" />
-                      <BatteryProgressNumber>{params.value.toString()}</BatteryProgressNumber>
-                    </Progressbar>
-                  </div>
-                ),
-              },
-              { field: 'bookings', headerName: 'Number of bookings' },
-              { field: 'type', headerName: 'Type' },
-              { field: 'position', headerName: 'Position', width: 300 },
-              {
-                field: '',
-                headerName: '',
-                width: 150,
-                renderCell: (params: ValueFormatterParams) => (
-                  <strong>
-                    <Button variant="outlined" color="secondary" size="small" style={{ marginLeft: 16 }}>
-                      Delete
-                    </Button>
-                  </strong>
-                ),
-              },
-            ]}
-            rows={vehicleRows}
-            disableColumnMenu
-            showToolbar
-            filterModel={vehicleFilterModel}
-          />
+    <>
+      <div style={{ height: 400, width: '100%' }}>
+        <div style={{ display: 'flex', height: '100%' }}>
+          <div style={{ flexGrow: 1 }}>
+            <DataGrid
+              columns={[
+                { field: 'id', hide: true },
+                {
+                  field: 'license_plate',
+                  headerName: 'License Plate',
+                  width: 150,
+                },
+                { field: 'status', headerName: 'Status', width: 150 },
+                {
+                  field: 'battery',
+                  headerName: 'Battery',
+                  width: 100,
+                  renderCell: (params: ValueFormatterParams) => (
+                    <div style={{ position: 'relative' }}>
+                      <Progressbar>
+                        <BatteryProgress value={params.value.toString()} max="100" />
+                        <BatteryProgressNumber>{params.value.toString()}</BatteryProgressNumber>
+                      </Progressbar>
+                    </div>
+                  ),
+                },
+                { field: 'bookings', headerName: 'Number of bookings' },
+                { field: 'type', headerName: 'Type' },
+                { field: 'position', headerName: 'Position', width: 300 },
+                {
+                  field: 'button',
+                  headerName: '',
+                  width: 150,
+                  renderCell: (params: ValueFormatterParams) => (
+                    <strong>
+                      <Button
+                        id={params.value.toString()}
+                        variant="outlined"
+                        color="secondary"
+                        size="small"
+                        onClick={handleDeleteVehicle}
+                        style={{ marginLeft: 16 }}
+                      >
+                        Delete
+                      </Button>
+                    </strong>
+                  ),
+                },
+              ]}
+              rows={vehicleRows}
+              disableColumnMenu
+              showToolbar
+              filterModel={vehicleFilterModel}
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          A Vehicle was sucessfully deleted!
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
