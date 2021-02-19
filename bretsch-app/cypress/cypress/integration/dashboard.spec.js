7d407f210902b62079476b1a0b23937c0e9d8f89
rico <ricardo.cincato@stud.h-da.de>
2021-02-19 18:55:08 +0100

    fixed dashboard test failed

--- bretsch-app/cypress/cypress/integration/dashboard.spec.js
+++ bretsch-app/cypress/cypress/integration/dashboard.spec.js
@@ -20,20 +20,20 @@ it("can login to the website", () => {
 
 it("can register new user", () => { 
     cy.visit("/");
-    cy.findByTestId(/login-button-id/i).click();
-    cy.findByTestId(/register-button-id/i).click();
+    cy.findByTestId(/login-button-id/i).click({ force: true });
+    cy.findByTestId(/register-button-id/i).click({ force: true });
     cy.findByLabelText(/Email Address/i).type(register.email);
     cy.findByTestId(/password-id1/i).type(register.password);
     cy.findByTestId(/confpassword-id2/i).type(register.password);
-    cy.findByTestId(/next-finish-id/i).click();
+    cy.findByTestId(/next-finish-id/i).click({ force: true });
     cy.findByLabelText(/First name/i).type(register.firstname);
     cy.findByLabelText(/Last name/i).type(register.lastname);
     cy.findByLabelText(/Birthdate/i).type(register.birthdate);
     cy.findByLabelText(/Street and Number/i).type(register.streetAndNumber);
     cy.findByLabelText(/City/i).type(register.city);
-    cy.findByTestId(/next-finish-id/i).click();
-    cy.findByTestId(/next-finish-id/i).click();
-    cy.findByTestId(/drawer-id/i).click();
+    cy.findByTestId(/next-finish-id/i).click({ force: true });
+    cy.findByTestId(/next-finish-id/i).click({ force: true });
+    cy.findByTestId(/drawer-id/i).click({ force: true });
     expect(cy.findByTestId(/user-name-id/i).should('have.text',`Logged in as ${register.firstname}.`));
     
 })
